c830cd990a777153fd1798573d789485c1bab436
Ignat Insarov <kindaro@gmail.com>
2021-03-15 18:53:58 +0500

    Run without errors, compile without warnings.

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan_core.h>
 
 struct point {int x; int y;};
-struct images {unsigned int const count; VkImage * const images; VkImageView * const views;};
+struct images
+{
+     unsigned int const count;
+     VkImage * const images;
+     VkImageView * const views;
+     VkFramebuffer * const frames;
+};
 struct devices
 {
      GLFWwindow * window;
@@ -18,6 +27,7 @@ struct devices
      VkSwapchainKHR chain;
      VkCommandPool pool;
      VkCommandBuffer buffer;
+     VkRenderPass renderPass;
      struct images * pointerToImages;
 };
 
@@ -28,6 +38,22 @@ void try (int const code, char const * const pointerToLocation)
           fprintf (stderr, "Error %d in %s!\n", code, pointerToLocation);
           exit (-1);
      }
+}
+
+void * xcalloc (unsigned int count, unsigned int size)
+{
+     void * pointerToMemory = calloc (count, size);
+     if (! pointerToMemory)
+     {
+          perror ("xcalloc");
+          exit (EXIT_FAILURE);
+     }
+     return pointerToMemory;
+}
+
+void * xmalloc0 (unsigned int size)
+{
+     return xcalloc (1, size);
 }
 
 void checkGlfwError (char const * const pointerToLocation)
@@ -54,11 +80,11 @@ void getLogicAndQueue (VkPhysicalDevice const card, VkSurfaceKHR const surface, 
      VkQueueFamilyProperties queueFamilies [numberOfAvailableQueueFamilies];
      vkGetPhysicalDeviceQueueFamilyProperties (card, &numberOfAvailableQueueFamilies, queueFamilies);
      {
-          * pointerToQueueFamilyIndex = -1;
-          for (unsigned int i = numberOfAvailableQueueFamilies - 1; i >= 0; i--)
+          * pointerToQueueFamilyIndex = ~ (unsigned int) 0;
+          for (unsigned int i = 0; i < numberOfAvailableQueueFamilies; ++i)
                if (queueFamilies [i].queueCount > 0 && queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
                {* pointerToQueueFamilyIndex = i; break;}
-          try (* pointerToQueueFamilyIndex == -1, "search for a queue");
+          try(*pointerToQueueFamilyIndex == ~ (unsigned int) 0, "search for a queue");
      }
      {
           float priority = 1;
@@ -126,7 +152,7 @@ VkSwapchainKHR getSwapchain (VkPhysicalDevice const card, VkDevice const logic, 
                 .imageColorSpace = format.colorSpace,
                 .imageExtent = {.width = size.x, .height = size.y},
                 .imageArrayLayers = 1,
-                .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT || VK_IMAGE_USAGE_STORAGE_BIT,
+                .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
                 .imageSharingMode = VK_SHARING_MODE_EXCLUSIVE,
                 .queueFamilyIndexCount = 0,
                 .pQueueFamilyIndices = NULL,
@@ -141,41 +167,65 @@ VkSwapchainKHR getSwapchain (VkPhysicalDevice const card, VkDevice const logic, 
      return chain;
 }
 
-struct images * getPointerToImages (VkDevice const logic, VkSwapchainKHR const swapchain, VkFormat const format)
+struct images * getPointerToImages (VkDevice const logic, VkSwapchainKHR const swapchain, VkFormat const format, VkRenderPass const renderPass, struct point const size)
 {
      unsigned int count;
      try (vkGetSwapchainImagesKHR (logic, swapchain, &count, NULL), "Get image count");
-     VkImage images [count];
+     VkImage * images = xcalloc (count, sizeof (VkImage));
      try (vkGetSwapchainImagesKHR (logic, swapchain, &count, images), "Get images");
-     VkImageView views [count];
-     for (unsigned int i = count; i == 0; --i)
+     VkImageView * views = xcalloc (count, sizeof (VkImageView));
+     VkFramebuffer * frames = xcalloc (count, sizeof (VkFramebuffer));
+     for (unsigned int i = 0; i < count; ++i)
      {
-          VkComponentMapping components =
-               {.r = VK_COMPONENT_SWIZZLE_R,
-                .g = VK_COMPONENT_SWIZZLE_G,
-                .b = VK_COMPONENT_SWIZZLE_B,
-                .a = VK_COMPONENT_SWIZZLE_A,
-               };
-          VkImageSubresourceRange subresourceRange =
-               {.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
-                .baseMipLevel = 0,
-                .levelCount = 1,
-                .baseArrayLayer = 0,
-                .layerCount = 1
-               };
-          VkImageViewCreateInfo info =
-               {.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
-                .pNext = NULL,
-                .flags = 0,
-                .image = images [i],
-                .viewType = VK_IMAGE_VIEW_TYPE_2D,
-                .format = format,
-                .components = components,
-                subresourceRange = subresourceRange,
-               };
-          try (vkCreateImageView (logic, & info, NULL, & views [i]), "Creating image view");
+          printf ("Initializing image %d.\n", i);
+          {
+               VkComponentMapping components =
+                    {.r = VK_COMPONENT_SWIZZLE_R,
+                     .g = VK_COMPONENT_SWIZZLE_G,
+                     .b = VK_COMPONENT_SWIZZLE_B,
+                     .a = VK_COMPONENT_SWIZZLE_A,
+                    };
+               VkImageSubresourceRange subresourceRange =
+                    {.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+                     .baseMipLevel = 0,
+                     .levelCount = 1,
+                     .baseArrayLayer = 0,
+                     .layerCount = 1
+                    };
+               VkImageViewCreateInfo info =
+                    {.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+                     .pNext = NULL,
+                     .flags = 0,
+                     .image = images [i],
+                     .viewType = VK_IMAGE_VIEW_TYPE_2D,
+                     .format = format,
+                     .components = components,
+                     .subresourceRange = subresourceRange,
+                    };
+               try (vkCreateImageView (logic, & info, NULL, & views [i]), "Creating image view");
+               printf ("Initializing image view %d at %p.\n", i, & images [i]);
+          }
+          {
+               VkFramebufferCreateInfo info =
+                    {.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+                     .pNext = NULL,
+                     .flags = 0,
+                     .renderPass = renderPass,
+                     .attachmentCount = 1,
+                     .pAttachments = & views [i],
+                     .width = size.x,
+                     .height = size.y,
+                     .layers = 1,
+                    };
+               try (vkCreateFramebuffer (logic, & info, NULL, & frames [i]), "Creating framebuffer");
+               printf ("Initializing framebuffer %d at %p.\n", i, & frames [i]);
+          }
      }
-     return & (struct images){.count = count, .images = images};
+     struct images * pointerToMemory = xmalloc0 (sizeof (struct images));
+     struct images temporaryStructure = {.count = count, .views = views, .images = images, .frames = frames};
+     memcpy (pointerToMemory, & temporaryStructure, sizeof (temporaryStructure));
+     printf ("pointer to views: %p.", pointerToMemory->views);
+     return pointerToMemory;
 }
 
 VkCommandPool getPool (VkDevice const logic, unsigned int const queueFamilyIndex)
@@ -185,7 +235,7 @@ VkCommandPool getPool (VkDevice const logic, unsigned int const queueFamilyIndex
           VkCommandPoolCreateInfo const info =
                {.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
                 .pNext = NULL,
-                .flags = 0,
+                .flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
                 .queueFamilyIndex = queueFamilyIndex,
                };
           try (vkCreateCommandPool (logic, &info, NULL, &pool), "Vulkan command pool acquisition");
@@ -209,9 +259,56 @@ VkCommandBuffer getBuffer (VkDevice const logic, VkCommandPool const pool)
      return buffer;
 }
 
-const struct devices enter (const struct point size)
+VkRenderPass getRenderPass (VkDevice const logic, VkFormat const format)
 {
-     struct devices devices = { };
+     VkRenderPass renderPass;
+     {
+          VkAttachmentDescription attachment =
+               {.flags = 0,
+                .format = format,
+                .samples = VK_SAMPLE_COUNT_1_BIT,
+                .loadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+                .storeOp = VK_ATTACHMENT_STORE_OP_STORE,
+                .stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+                .stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+                .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+                .finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
+               };
+          VkAttachmentReference reference =
+               {.attachment = 0,
+                .layout = VK_IMAGE_LAYOUT_GENERAL,
+               };
+          VkSubpassDescription subpass =
+               {.flags = 0,
+                .pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS,
+                .inputAttachmentCount = 0,
+                .pInputAttachments = NULL,
+                .colorAttachmentCount = 1,
+                .pColorAttachments = & reference,
+                .pResolveAttachments = NULL,
+                .pDepthStencilAttachment = NULL,
+                .preserveAttachmentCount = 0,
+                .pPreserveAttachments = NULL,
+               };
+          VkRenderPassCreateInfo info =
+               {.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
+                .pNext = NULL,
+                .flags = 0,
+                .attachmentCount = 1,
+                .pAttachments = & attachment,
+                .subpassCount = 1,
+                .pSubpasses = & subpass,
+                .dependencyCount = 0,
+                .pDependencies = NULL,
+               };
+          try (vkCreateRenderPass (logic, & info, NULL, & renderPass), "Create render pass");
+     }
+     return renderPass;
+}
+
+struct devices enter (const struct point size)
+{
+     struct devices devices;
      if (! glfwInit ( )) exit (-1);
      glfwWindowHint (GLFW_CLIENT_API, GLFW_NO_API);
      glfwWindowHint (GLFW_RESIZABLE, GLFW_FALSE);
@@ -238,7 +335,7 @@ const struct devices enter (const struct point size)
                 .ppEnabledExtensionNames = enabledExtensionNames,
                };
           checkGlfwError ("Query Vulkan extensions");
-          for (int i = info.enabledExtensionCount - 1; i >= 0; i--) printf ("Required extension: %s.\n", info.ppEnabledExtensionNames [i]);
+          for (unsigned int i = 0; i < info.enabledExtensionCount; ++i) printf ("Required extension: %s.\n", info.ppEnabledExtensionNames [i]);
           try (vkCreateInstance(&info, NULL, &devices.vulkan), "Vulkan initialization");
      }
      try (glfwCreateWindowSurface (devices.vulkan, devices.window, NULL, &devices.surface), "Vulkan surface initialization");
@@ -248,17 +345,27 @@ const struct devices enter (const struct point size)
      devices.chain = getSwapchain (devices.card, devices.logic, devices.surface, devices.format, size);
      devices.pool = getPool (devices.logic, devices.queueFamilyIndex);
      devices.buffer = getBuffer (devices.logic, devices.pool);
-     devices.pointerToImages = getPointerToImages (devices.logic, devices.chain, devices.format.format);
+     devices.renderPass = getRenderPass (devices.logic, devices.format.format);
+     devices.pointerToImages = getPointerToImages (devices.logic, devices.chain, devices.format.format, devices.renderPass, size);
      printf ("Images in the swap chain: %d\n", devices.pointerToImages->count);
      return devices;
 }
 
 int leave (const struct devices devices)
 {
+     try (vkDeviceWaitIdle (devices.logic), "waiting for device to finish work");
+     for (unsigned int i = 0; i < devices.pointerToImages->count; ++i)
+     {
+          vkDestroyImageView (devices.logic, devices.pointerToImages->views [i], NULL);
+          vkDestroyFramebuffer (devices.logic, devices.pointerToImages->frames [i], NULL);
+     }
+     free (devices.pointerToImages->images);
+     free (devices.pointerToImages->views);
+     free (devices.pointerToImages->frames);
      vkDestroyCommandPool (devices.logic, devices.pool, NULL);
+     vkDestroyRenderPass (devices.logic, devices.renderPass, NULL);
      vkDestroySwapchainKHR (devices.logic, devices.chain, NULL);
      vkDestroySurfaceKHR (devices.vulkan, devices.surface, NULL);
-     try (vkDeviceWaitIdle (devices.logic), "waiting for device to finish work");
      vkDestroyDevice (devices.logic, NULL);
      vkDestroyInstance (devices.vulkan, NULL);
      glfwDestroyWindow (devices.window);
@@ -266,12 +373,23 @@ int leave (const struct devices devices)
      return 0;
 }
 
-void mainLoop (const struct devices devices)
+void mainLoop (const struct devices devices, struct point const size)
 {
+     static unsigned int mainLoopCounter = 0;
      glfwSwapBuffers (devices.window);
      glfwPollEvents ( );
      unsigned int imageIndex;
-     try (vkAcquireNextImageKHR (devices.logic, devices.chain, -1, VK_NULL_HANDLE, VK_NULL_HANDLE, &imageIndex), "Acquiring next image");
+     {
+          VkFence fence;
+          {
+               VkFenceCreateInfo info = {.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO, .pNext = NULL, .flags = 0};
+               try (vkCreateFence (devices.logic, & info, NULL, & fence), "Get a fence");
+          }
+          try (vkAcquireNextImageKHR (devices.logic, devices.chain, -1, VK_NULL_HANDLE, fence, &imageIndex), "Acquiring next image");
+          try (vkWaitForFences (devices.logic, 1, & fence, VK_TRUE, -1), "Waiting for image acquisition");
+          vkDestroyFence (devices.logic, fence, NULL);
+          printf ("Image view acquired: %d at %p.\n", imageIndex, & devices.pointerToImages->images [imageIndex]);
+     }
      unsigned int imageIndices [ ] = {imageIndex};
      {
           VkCommandBufferBeginInfo info =
@@ -281,6 +399,20 @@ void mainLoop (const struct devices devices)
                 .pInheritanceInfo = NULL,
                };
           try (vkBeginCommandBuffer (devices.buffer, &info), "Begin buffer");
+          printf ("Framebuffer pointer: %p.\n", & devices.pointerToImages->frames [imageIndex]);
+          {
+               VkRenderPassBeginInfo info =
+                    {.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
+                     .pNext = NULL,
+                     .renderPass = devices.renderPass,
+                     .framebuffer = devices.pointerToImages->frames [imageIndex],
+                     .renderArea = (VkRect2D) {.offset.x = 0, .offset.y = 0, .extent.width = size.x, .extent.height = size.y},
+                     .clearValueCount = 0,
+                     .pClearValues = NULL,
+                    };
+               vkCmdBeginRenderPass (devices.buffer, & info, VK_SUBPASS_CONTENTS_INLINE);
+               vkCmdEndRenderPass (devices.buffer);
+          }
           try (vkEndCommandBuffer (devices.buffer), "End buffer");
      }
      {
@@ -311,12 +443,14 @@ void mainLoop (const struct devices devices)
           try (vkQueuePresentKHR (devices.queue, &info), "Presentation");
      }
      try (vkQueueWaitIdle (devices.queue), "Waiting for the queue to become idle");
+     sleep (1);
+     mainLoopCounter++;
 }
 
 int main (void)
 {
      const struct point size = {.x = 800, .y = 600};
      const struct devices devices = enter (size);
-     while (! glfwWindowShouldClose (devices.window)) mainLoop (devices);
+     while (! glfwWindowShouldClose (devices.window)) mainLoop (devices, size);
      return leave (devices);
 }
